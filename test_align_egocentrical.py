95d0cff5bb0c8de80288accdc2a2be45766011ed
John Zhou <john.ly.zhou@gmail.com>
2021-07-19 13:41:03 -0400

    remove background subtraction

---
+++ test_align_egocentrical.py
@@ -0,0 +1,22 @@
+import unittest
+import vame
+
+class TestAlignmentMethods(unittest.TestCase):
+    
+    self.test_image = np.zeros((480, 640, 3))
+    
+    def test_crop_and_flip(self):
+        self.assertTrue(True)
+        
+    def test_background(self):
+        pass
+    
+    def test_align_mouse(self):
+        pass
+    
+    def test_save_aligned_video(self):
+        pass
+    
+    
+if __name__ == '__main__':
+    unittest.main()
