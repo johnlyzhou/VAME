7c2687a0fd9c2f5600888b7c4947b811c2c7baf8
John Zhou <john.ly.zhou@gmail.com>
2021-08-18 15:23:39 -0400

    automate vame egocentric alignment testing

--- run.py
+++ run.py
@@ -1,22 +1,23 @@
 import vame
 import os
+import datetime
 
 # These paths have to be set manually
 working_directory = '/Users/johnzhou/code'
 project = 'ps_vae'
-videos = ['/Users/johnzhou/code/dlc_videos'
-          '/B125_precon_topDLC_resnet101_FearConditioningTopNov27shuffle1_950000_filtered.mp4']
-csv = '/Users/johnzhou/code/dlc_videos/B125_precon_topDLC_resnet101_FearConditioningTopNov27shuffle1_950000_filtered' \
-      '.csv '
+videos = ['/Users/johnzhou/code/VAME/results/310_480_masked.mp4']
+csv = '/Users/johnzhou/code/VAME/results/310_480_masked.csv'
+
+mydate = datetime.datetime.now()
+short_month = mydate.strftime("%b")
+date = mydate.date().strftime("%-d")
 
 # Initialize your project
 config = vame.init_new_project(
     project=project, videos=videos, working_directory=working_directory, videotype='.mp4')
-os.system('cp /Users/johnzhou/code/dlc_videos'
-          '/B125_precon_topDLC_resnet101_FearConditioningTopNov27shuffle1_950000_filtered.csv '
-          '/Users/johnzhou/code/ps_vae-Aug05-2021/videos/pose_estimation')
+os.system('cp {} /Users/johnzhou/code/ps_vae-{}{}-2021/videos/pose_estimation'.format(csv, short_month, date))
 
 # Run egocentric alignment and save video
 vame.egocentric_alignment(
-    config, pose_ref_index=[0, 4], use_video=True, check_video=True, save_video=True)
-os.system('rm -rf /Users/johnzhou/code/ps_vae-Aug05-2021')
+    config, pose_ref_index=[0, 3], crop_size=(192, 192), use_video=True, check_video=False, save_video=True)
+os.system('rm -rf /Users/johnzhou/code/ps_vae-{}{}-2021'.format(short_month, date))
